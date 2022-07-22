f4b4002bddf792ab3972b07200305ceca2af36b7
whimsicaltestical <jamgriffin7@gmail.com>
2022-07-22 15:36:40 -0500

    Add button for grid creation script

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let r;
 let g;
 let b;
-
-
 const container = document.querySelector('#container');
+const gridItems = container.querySelectorAll('.item');
+const button = document.querySelector('button');
+
+button.addEventListener('click', (e) => {  // add prompt for grid number, create grid and modify for loop based on prompt
+
+})
+
 for (let i = 0; i < 256; i++) {
   const div = document.createElement('div');
   div.setAttribute('style', `grid-column: auto; grid-row: auto;`);
@@ -15,16 +20,15 @@ for (let i = 0; i < 256; i++) {
 function randomizeColor() {
     r = Math.random() * 255;
     g = Math.random() * 255;
-    b = Math.random() * 255
+    b = Math.random() * 255;
 }
 
-const gridItems = container.querySelectorAll('.item');
-console.log(gridItems);
+
 gridItems.forEach(gridItem => { // idk why this works, don't erase
     gridItem.addEventListener('mouseover', (e) => {
         randomizeColor();
         gridItem.setAttribute('style',
-         `color: transparent; background-color: rgb(${r}, ${g}, ${b});`)
-    })
-    gridItem.addEventListener('dblclick', (e) =>{gridItem.setAttribute('style', 'background-color: transparent')})
+         `color: transparent; background-color: rgb(${r}, ${g}, ${b});`);
+    });
+    gridItem.addEventListener('dblclick', (e) =>{gridItem.setAttribute('style', 'background-color: transparent')});
 })
