7d78065d55f86e98cb8fe8b0bae47e7f56f7c863
whimsicaltestical <jamgriffin7@gmail.com>
2022-07-22 09:10:18 -0500

    Add script to change state of divs when hovered over

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const container = document.querySelector('#container');
 for (let i = 0; i < 256; i++) {
   const div = document.createElement('div');
-  div.setAttribute('style', `grid-column: auto; grid-row: auto;`); 
+  div.setAttribute('style', `grid-column: auto; grid-row: auto;`);
+  div.classList.add('item'); 
   div.textContent = "Hover Over Me";
   container.appendChild(div);
 }
+
+const gridItems = container.querySelectorAll('.item');
+console.log(gridItems);
+gridItems.forEach(gridItem => { // idk why this works, don't erase
+    gridItem.addEventListener('mouseover', (e) => {
+        console.log(e);
+        gridItem.setAttribute('style', 'color: transparent;')
+    })
+})
