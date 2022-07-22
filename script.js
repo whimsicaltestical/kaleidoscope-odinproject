37f23d27f5da02c1543d2a635d4bc6363c035cd0
whimsicaltestical <jamgriffin7@gmail.com>
2022-07-22 16:52:46 -0500

    Add script to generate upto 100x100 grids

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
 let r;
 let g;
 let b;
+let gridArea;
 const container = document.querySelector('#container');
-const gridItems = container.querySelectorAll('.item');
 const button = document.querySelector('button');
-
-button.addEventListener('click', (e) => {  // add prompt for grid number, create grid and modify for loop based on prompt
-
-})
-
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement('div');
-  div.setAttribute('style', `grid-column: auto; grid-row: auto;`);
-  div.classList.add('item'); 
-  div.textContent = "Hover Over Me";
-  container.appendChild(div);
-}
 
 function randomizeColor() {
     r = Math.random() * 255;
@@ -23,12 +11,36 @@ function randomizeColor() {
     b = Math.random() * 255;
 }
 
-
-gridItems.forEach(gridItem => { // idk why this works, don't erase
-    gridItem.addEventListener('mouseover', (e) => {
-        randomizeColor();
-        gridItem.setAttribute('style',
-         `color: transparent; background-color: rgb(${r}, ${g}, ${b});`);
-    });
-    gridItem.addEventListener('dblclick', (e) =>{gridItem.setAttribute('style', 'background-color: transparent')});
+button.addEventListener('click', (e) => {  // look into getting grid content to only fit size of container
+    gridArea = window.prompt("Enter the number of rows and columns you wish for the grid, less than or equal to 100", "16")
+    while (gridArea > 100) {
+        gridArea = window.prompt("Please enter a number less than or equal to 100", "100");
+    }
+    container.setAttribute('style', `grid-template-columns: repeat(${gridArea}, 1fr); grid-template-rows: repeat(${gridArea}, 1fr);`)
+    for (let i = 0; i < (gridArea * gridArea); i++) {
+        const div = document.createElement('div');
+        div.setAttribute('style', `grid-column: auto; grid-row: auto;`);
+        div.classList.add('item'); 
+        div.textContent = "Hover Over Me";
+        container.appendChild(div);
+    }
+    const gridItems = container.querySelectorAll('.item');
+    gridItems.forEach(gridItem => { // idk why this works, don't erase
+        gridItem.addEventListener('mouseover', (e) => {
+            randomizeColor();
+            gridItem.setAttribute('style',
+             `color: transparent; background-color: rgb(${r}, ${g}, ${b});`);
+        });
+        gridItem.addEventListener('dblclick', (e) =>{gridItem.setAttribute('style', 'background-color: transparent')});
+    })
 })
+
+//for (let i = 0; i < 256; i++) {
+  //const div = document.createElement('div');
+ // div.setAttribute('style', `grid-column: auto; grid-row: auto;`);
+  //div.classList.add('item'); 
+  //div.textContent = "Hover Over Me";
+  //container.appendChild(div);
+//}
+
+
